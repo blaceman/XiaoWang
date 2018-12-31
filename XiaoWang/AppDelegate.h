a07e12f08fcc78937dad231b051ff1960c29f74f
luowx <luowx@figo.cn>
2018-12-31 18:19:14 +0800

    Initial Commit

---
+++ XiaoWang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoWang
+//
+//  Created by blaceman on 2018/12/31.
+//  Copyright © 2018年 new4545. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
